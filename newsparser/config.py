4baf06bb8e2e63258eeae34056b496a23db25523
Soubhagya Nayak <soubhagya.r.nayak@gmail.com>
2018-11-08 20:08:10 +0530

    added support for postgre SQL as a persistent storage

---
+++ newsparser/config.py
@@ -0,0 +1,14 @@
+DATABASE_CONFIG = {
+    'type': 'sqlite',
+    'dbname': 'feeds.db'
+}
+
+'''
+DATABASE_CONFIG = {
+    'type': 'postgre',
+    'host': 'localhost',
+    'dbname': 'feeds',
+    'user':'user',
+    'password':'password',
+}
+'''
--- newsparser/feed_manager.py
+++ newsparser/feed_manager.py
@@ -1,7 +1,26 @@
 import sqlite3
 from sqlite3 import Error
 
-DATABASE_NAME = 'feeds.db'
+import newsparser.config as config
+
+SqliteQueries = {
+    'add_feed':'INSERT INTO Feeds(URL,Description) VALUES (?,?)',
+    'get_feeds':'SELECT ROWID,URL,Description FROM Feeds',
+    'create_feeds_table':'CREATE TABLE IF NOT EXISTS Feeds (URL TEXT UNIQUE, Description TEXT);',
+    'create_feed_entries_table': 'CREATE TABLE IF NOT EXISTS FeedEntries (Link TEXT UNIQUE, Title TEXT, Description TEXT, GUID TEXT, PubDate TEXT, FeedId INTEGER);',
+    'delete_feed_entries' : 'DELETE FROM FeedEntries WHERE FeedId = (SELECT ROWID FROM Feeds WHERE Feeds.URL=?)',
+    'delete_feeds':'DELETE FROM Feeds WHERE Feeds.URL=?'
+}
+
+PostgreQueries = {
+    'add_feed':'INSERT INTO Feeds(URL,Description) VALUES (%s,%s)',
+    'get_feeds': 'SELECT ROWID,URL,Description FROM Feeds',
+    'create_feeds_table':'CREATE TABLE IF NOT EXISTS Feeds (ROWID SERIAL, URL TEXT UNIQUE, Description TEXT);',
+    'create_feed_entries_table': 'CREATE TABLE IF NOT EXISTS FeedEntries (ROWID SERIAL, Link TEXT UNIQUE, Title TEXT, Description TEXT, GUID TEXT, PubDate TEXT, FeedId INTEGER);',
+    'delete_feed_entries' : 'DELETE FROM FeedEntries WHERE FeedId = (SELECT ROWID FROM Feeds WHERE Feeds.URL=%s)',
+    'delete_feeds':'DELETE FROM Feeds WHERE Feeds.URL=%s'
+}
+
 
 class Feed:
     def __str__(self):
@@ -16,8 +35,7 @@ class FeedManager:
         connection = self._get_connection()
         with connection:
             cursor = connection.cursor()
-            cursor.execute("INSERT INTO Feeds(URL,Description) VALUES (?,?)",
-                           (url,description))
+            cursor.execute(self._get_query('add_feed'), (url,description))
             
     def remove_feed(self,url):
         connection = self._get_connection()
@@ -25,16 +43,19 @@ class FeedManager:
             cursor = connection.cursor()
             #TODO: make it atomic
             cursor.execute(
-                "DELETE FROM FeedEntries WHERE FeedId = (SELECT ROWID FROM Feeds WHERE Feeds.URL=?)",
-                (url,))
-            cursor.execute("DELETE FROM Feeds WHERE Feeds.URL=?",(url,))
+                self._get_query('delete_feed_entries'),(url,))
+            cursor.execute(self._get_query('delete_feeds'),(url,))
             
     def get_feeds(self):
         connection = self._get_connection()
         feeds = []
         with connection:
             cursor = connection.cursor()
-            for row in cursor.execute("SELECT ROWID,URL,Description FROM Feeds"):
+            cursor.execute("SELECT ROWID,URL,Description FROM Feeds")
+            while True:
+                row = cursor.fetchone()
+                if row == None:
+                    break
                 feed = Feed()
                 feed.id = row[0]
                 feed.url = row[1]
@@ -53,13 +74,35 @@ class FeedManager:
         connection = self._get_connection()
         with connection:
             cursor = connection.cursor()
-            cursor.execute("CREATE TABLE IF NOT EXISTS Feeds (URL TEXT UNIQUE, Description TEXT);")
-            cursor.execute("CREATE TABLE IF NOT EXISTS FeedEntries (Link TEXT UNIQUE, Title TEXT, Description TEXT, GUID TEXT, PubDate TEXT, FeedId INTEGER);")
+            cursor.execute(self._get_query('create_feeds_table'))
+            cursor.execute(self._get_query('create_feed_entries_table'))
     
     def _get_connection(self):
         connection = None
         try:
-            connection = sqlite3.connect(DATABASE_NAME)
+            if config.DATABASE_CONFIG['type'] == 'sqlite':
+                connection = sqlite3.connect(config.DATABASE_CONFIG['dbname'])
+            elif config.DATABASE_CONFIG['type'] == 'postgre':
+                import psycopg2
+                user = config.DATABASE_CONFIG['user']
+                password = config.DATABASE_CONFIG['password']
+                dbname = config.DATABASE_CONFIG['dbname']
+                connection = psycopg2.connect("dbname='{}' user='{}' password='{}'".format(dbname,user,password))
+            else:
+                raise RuntimeError("The specified db type:{} is not supported".format(config.DATABASE_CONFIG['type']))
         except Error as e:
             print(e)
         return connection
+    
+    def _get_query(self,query_id):
+        query = None
+        try:
+            if config.DATABASE_CONFIG['type'] == 'sqlite':
+                query = SqliteQueries[query_id]
+            elif config.DATABASE_CONFIG['type'] == 'postgre':
+                query = PostgreQueries[query_id]
+            else:
+                raise RuntimeError("The specified db type:{} is not supported".format(config.DATABASE_CONFIG['type']))
+        except Error as e:
+            print(e)
+        return query
