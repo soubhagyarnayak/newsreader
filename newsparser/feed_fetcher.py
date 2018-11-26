e0191d12f54073c0db36e4fa71c0b2216d56125a
Soubhagya Nayak <soubhagya.r.nayak@gmail.com>
2018-11-26 18:27:18 +0530

    Added RssFeedReader class

--- newsparser/feed_fetcher.py
+++ newsparser/feed_fetcher.py
@@ -5,22 +5,26 @@ class FeedItem:
     def __str__(self):
         return "title:{0},description:{1},link:{2}".format(self.title,self.description,self.link)
 
-def get_feeds(url):
-    feeds_content = requests.get(url)
-    xml = etree.XML(feeds_content.content)
-    feeds = []
-    for item in xml.iter("item"):
-        feedItem = FeedItem()
-        for element in item:
-            if element.tag == "title":
-                feedItem.title = element.text
-            elif element.tag == "description":
-                feedItem.description = element.text
-            elif element.tag == "guid":
-                feedItem.guid = element.text
-            elif element.tag == "pubDate":
-                feedItem.publication_date = element.text
-            elif element.tag == "link":
-                feedItem.link = element.text
-        feeds.append(feedItem)
-    return feeds
+class RssFeedFetcher:
+    def get_feeds(self, url):
+        print(url)
+        feeds_content = requests.get(url)
+        xml = etree.XML(feeds_content.content)
+        feeds = []
+        for item in xml.iter("item"):
+            feedItem = FeedItem()
+            for element in item:
+                if element.tag == "title":
+                    feedItem.title = element.text
+                elif element.tag == "description":
+                    feedItem.description = element.text
+                elif element.tag == "guid":
+                    feedItem.guid = element.text
+                elif element.tag == "pubDate":
+                    feedItem.publication_date = element.text
+                elif element.tag == "link":
+                    feedItem.link = element.text
+            if not hasattr(feedItem,'guid'):
+                feedItem.guid = feedItem.link
+            feeds.append(feedItem)
+        return feeds
