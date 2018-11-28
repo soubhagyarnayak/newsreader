db3bb010460c046a93024e1adf6638a24968e44a
Soubhagya Nayak <soubhagya.r.nayak@gmail.com>
2018-11-28 22:07:53 +0530

    Handled exceptions in processing feeds and rabbitmq message handler

--- newsparser/oped_manager.py
+++ newsparser/oped_manager.py
@@ -7,5 +7,9 @@ class OpEdManager:
         oped_categories = oped_store.get_categories()
         parser = RssFeedFetcher()
         for oped_category in oped_categories:
-            articles = parser.get_feeds(oped_category.link)
-            oped_store.add_articles(oped_category,articles)
+            try:
+                articles = parser.get_feeds(oped_category.link)
+                oped_store.add_articles(oped_category,articles)
+            except Exception as e:
+                print("Encountered exception while processing op ed feeds.")
+                print(e)
--- newsparser/task_processor.py
+++ newsparser/task_processor.py
@@ -7,19 +7,25 @@ from oped_manager import OpEdManager
 TASK_PROCESSOR_QUEUE_NAME = 'newsparser'
 
 def _handler(channel,method,properties,body):
-        body = body.decode('utf8').replace("'", '"')
-        print('Processing message:'+body)
-        message = json.loads(body)
-        if message['command'] == 'processHN':
-            print('Processing HN')
-            hn = HackerNewsManager()
-            hn.process()
-        elif message['command'] == 'processOpEd':
-            print('Processing OpEd')
-            oped_manager = OpEdManager()
-            oped_manager.process()
-        else:
-            print("Command:{} is not supported".format(message.command))
+        try:
+            body = body.decode('utf8').replace("'", '"')
+            print('Processing message:'+body)
+            message = json.loads(body)
+            if message['command'] == 'processHN':
+                print('Processing HN')
+                hn = HackerNewsManager()
+                hn.process()
+            elif message['command'] == 'processOpEd':
+                print('Processing OpEd')
+                oped_manager = OpEdManager()
+                oped_manager.process()
+            else:
+                print("Command:{} is not supported".format(message.command))
+            print('Processing completed successfully.')
+        except Exception as e:
+            print(e)
+            print('Processing completed with exception.')
+        
 
 class TaskProcessor():
     def __init__(self):
