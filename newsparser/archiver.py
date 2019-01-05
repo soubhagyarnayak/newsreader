214bac4a4aafefa0562902af42cbf403f0b4c21f
Soubhagya Nayak <soubhagya.r.nayak@gmail.com>
2019-01-05 23:44:35 +0530

    added a fetcher to fetch and archive Dharatri epaper

--- newsparser/archiver.py
+++ newsparser/archiver.py
@@ -1,10 +1,11 @@
 import os
 
 from feed_fetcher import HtmlFetcher
-from custom_fetcher import SamajaFetcher
+from custom_fetcher import SamajaFetcher, DharitriFetcher
 
 MAIN_FILE_NAME = "main.html"
 SAMAJA_DIR_PATH = "samaja"
+DHARITRI_DIR_PATH = "dharitri"
 
 class Archiver:
     def archive_webpage(self,url,directory):
@@ -15,21 +16,32 @@ class Archiver:
         html_content = html_fetcher.get_raw_content(url)
         with open(filepath,'w') as file:
             file.write(html_content.prettify())
+
     def archive_samaja(self,date):
-        samaja_fetcher = SamajaFetcher()
-        image_contents = samaja_fetcher.fetch(date)
-        if not os.path.exists(SAMAJA_DIR_PATH):
-            os.makedirs(SAMAJA_DIR_PATH)
-        subdir = os.path.join(SAMAJA_DIR_PATH,date.strftime("%d%m%Y"))
-        if not os.path.exists(subdir):
-            os.makedirs(subdir)
+        self._archive(SamajaFetcher(),SAMAJA_DIR_PATH,date)
+
+    def archive_dharitri(self,date):
+        self._archive(DharitriFetcher(),DHARITRI_DIR_PATH,date)
+    
+    def _archive(self,fetcher,parent_dir_path,date):
+        image_contents = fetcher.fetch(date)
+        subdir = self.ensure_directory(parent_dir_path, date)
         page_num = 1
         for image_content in image_contents:
             filepath = os.path.join(subdir,"{}.jpg".format(page_num))
             with open(filepath,'wb') as file:
                 file.write(image_content)
             page_num += 1
+
+    def ensure_directory(self,parent,date):
+        if not os.path.exists(parent):
+            os.makedirs(parent)
+        subdir = os.path.join(parent,date.strftime("%d%m%Y"))
+        if not os.path.exists(subdir):
+            os.makedirs(subdir)
+        return subdir
+
 '''archiver = Archiver()
 from datetime import datetime
-archiver.archive_samaja(datetime.now())'''
-        
+#archiver.archive_samaja(datetime.now())
+archiver.archive_dharitri(datetime.now())'''
--- newsparser/custom_fetcher.py
+++ newsparser/custom_fetcher.py
@@ -1,4 +1,5 @@
 import requests
+from bs4 import BeautifulSoup
 
 class SamajaFetcher:
     def fetch(self, date):
@@ -14,3 +15,34 @@ class SamajaFetcher:
             pages.append(response.content)
             page_num += 1
         return pages
+
+class DharitriFetcher:
+    def fetch(self, date):
+        pages = []
+        bbsr_edition_url = self.find_bbsr_url(date)
+        print(bbsr_edition_url)
+        num_pages = self.find_num_pages(bbsr_edition_url)
+        for i in range(num_pages):
+            response = requests.get(bbsr_edition_url+"/page/"+str(i+1))
+            print_img = BeautifulSoup(response.text,features="lxml").find(id='print_img')
+            response = requests.get(print_img['src'])
+            pages.append(response.content)
+        return pages
+
+    def find_bbsr_url(self,date):
+        archive_url= "http://dharitriepaper.in/archive/{}".format(date.strftime("%Y-%m-%d"))
+        response = requests.get(archive_url)
+        content = BeautifulSoup(response.text,features="lxml")
+        anchors = content.find_all('a')
+        bbsr_hrefs = [anchor['href'] for anchor in anchors if ('bhubaneswar' in anchor['href'] and 'edition' in anchor['href'])]
+        return "http://dharitriepaper.in"+bbsr_hrefs[0]
+    
+    def find_num_pages(self,edition_url):
+        response = requests.get(edition_url)
+        ddlistPage = BeautifulSoup(response.text,features="lxml").find(id="ddlistPage")
+        options = ddlistPage.find_all('option')
+        return len(options)
+
+class SambadaFetcher:
+    def fetch(self, date):
+        pass
