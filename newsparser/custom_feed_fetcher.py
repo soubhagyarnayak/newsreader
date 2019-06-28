751dbd28f1a9084e1a4838781c21200f5cde5fb7
soubhagya <soubhagya.r.nayak@gmail.com>
2019-06-28 17:57:34 +0530

    added a custom handling to get opeds of ToI

---
+++ newsparser/custom_feed_fetcher.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup
+import requests
+from datetime import date
+
+from feed_fetcher import FeedItem
+
+class ToiOpinionFetcher:
+    def get_feeds(self,url):
+        feeds = []
+        feeds_content = requests.get(url)
+        content = BeautifulSoup(feeds_content.text, features="lxml")
+        headings = content.findAll("h2",{"class":"media-heading"})
+        for heading in headings:
+            a = heading.findAll("a")[0]
+            feed = FeedItem()
+            feed.title = a.get("title")
+            feed.link = a.get("href")
+            feed.description = feed.title
+            feed.publication_date = date.today().strftime("%m/%d/%Y")
+            feed.guid = feed.link
+            feeds.append(feed)
+        return feeds
--- newsparser/oped_manager.py
+++ newsparser/oped_manager.py
@@ -2,29 +2,42 @@ from tqdm import tqdm
 from oped_store import OpEdStore
 from feed_fetcher import RssFeedFetcher, HtmlFetcher
 from text_analyzer import TextAnalyzer
+from custom_feed_fetcher import ToiOpinionFetcher
 
 class OpEdManager:
+    def __init__(self):
+        self.html_fetcher = HtmlFetcher()
+        self.text_analyzer = TextAnalyzer()
+        self.oped_store = OpEdStore()
+
     def process(self):
-        oped_store = OpEdStore()
-        oped_categories = oped_store.get_categories()
+        oped_categories = self.oped_store.get_categories()
         parser = RssFeedFetcher()
-        html_fetcher = HtmlFetcher()
-        text_analyzer = TextAnalyzer()
+        toi_oped_parser = ToiOpinionFetcher()
         for oped_category in tqdm(oped_categories, total=len(oped_categories)):
             try:
-                articles = parser.get_feeds(oped_category.link)
-                for article in articles:
-                    try:
-                        paragraphs = html_fetcher.get_paragraphs(article.link)
-                        text = ' '.join(paragraphs)
-                        article.keywords = text_analyzer.get_keywords(text)
-                        article.summary = text_analyzer.get_summary(text)
-                    except Exception as e:
-                        print("Encountered exception while fetching and processing article.")
-                        print(e)
-                        break
-                oped_store.add_articles(oped_category,articles)
+                if(oped_category.id==1):
+                    articles = toi_oped_parser.get_feeds('https://timesofindia.indiatimes.com/blogs/')
+                    self.process_articles(oped_category, articles)
+                else:
+                    articles = parser.get_feeds(oped_category.link)
+                self.process_articles(oped_category, articles)
             except Exception as e:
                 print("Encountered exception while processing op ed feeds.")
                 print(e)
-                break
+                print(oped_category)
+
+    def process_articles(self,oped_category,articles):
+        for article in articles:
+            try:
+                paragraphs = self.html_fetcher.get_paragraphs(article.link)
+                text = ' '.join(paragraphs)
+                article.keywords = self.text_analyzer.get_keywords(text)
+                article.summary = self.text_analyzer.get_summary(text)
+            except Exception as e:
+                print("Encountered exception while fetching and processing article.")
+                print(e)
+                print(article)
+        self.oped_store.add_articles(oped_category,articles)
+
+
