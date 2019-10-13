49076db6b4ee5e27f52d783f288187e6b8f06ee3
soubhagya <soubhagya.r.nayak@gmail.com>
2019-10-14 01:17:44 +0530

    added the code to split the news paper view into horizontal sections

--- newsparser/news_image_parser.py
+++ newsparser/news_image_parser.py
@@ -9,12 +9,8 @@ class ImageUtility:
         Trims an image i.e. removes similar pixels from all sides.
         If convert_to_black_white is specified, then the image is first processed to have better result
         """
-        if convert_to_black_white:
-            cannonical_image = self.convert_to_black_white(src_image)
-        else:
-            cannonical_image = src_image
-        bitmaps = cannonical_image.load()
-        (x_left,y_top,x_right,y_bottom) = (0,0,cannonical_image.size[0],cannonical_image.size[1])
+        (bitmaps,X,Y) = self.load_bitmap(src_image, convert_to_black_white)
+        (x_left,y_top,x_right,y_bottom) = (0,0,X,Y)
 
         all_white = True
         while True:
@@ -64,17 +60,13 @@ class ImageUtility:
         trimmed_image = src_image.crop(box)
         return trimmed_image
 
-    def split_x(self, src_image, convert_to_black_white):
+    def split_x(self, src_image, convert_to_black_white=False):
         """
         Splits an image vertically if all pixels in a column are same
         If convert_to_black_white is specified, then the image is first processed to have better result
         """
-        if convert_to_black_white:
-            cannonical_image = self.convert_to_black_white(src_image)
-        else:
-            cannonical_image = src_image
-        bitmaps = cannonical_image.load()
-        (x_left,y_top,x_right,y_bottom) = (0,0,cannonical_image.size[0],cannonical_image.size[1])
+        (bitmaps,X,Y) = self.load_bitmap(src_image, convert_to_black_white)
+        (x_left,y_top,x_right,y_bottom) = (0,0,X,Y)
         regions = [(x_left,x_right)]
         for x in range(x_left,x_right-1):
             all_equal = True
@@ -89,6 +81,33 @@ class ImageUtility:
         boxes = [(region[0],y_top,region[1],y_bottom) for region in regions if (region[1]-region[0])>1]
         return [src_image.crop(box) for box in boxes]
 
+    def split_y(self, src_image, convert_to_black_white=False):
+        """
+        Splits an image horizontally if all pixels in a row are same
+        If convert_to_black_white is specified, then the image is first processed to have better result
+        """
+        (bitmaps,X,Y) = self.load_bitmap(src_image, convert_to_black_white)
+        (x_left,y_top,x_right,y_bottom) = (0,0,X,Y)
+        regions = [(y_top, y_bottom)]
+        for y in range(y_top,y_bottom-1):
+            all_equal = True
+            for x in range(x_left+1,x_right):
+                if bitmaps[x,y] != bitmaps[x_left,y]:
+                    all_equal = False
+                    break
+            if all_equal:
+                last_region = regions[-1]
+                regions[-1] = (last_region[0],y-1)
+                regions.append((y+1,last_region[1]))
+        boxes = [(x_left,region[0],x_right,region[1]) for region in regions if (region[1]-region[0])>1]
+        return [src_image.crop(box) for box in boxes]
+
+    def load_bitmap(self, src_image, convert_to_black_white):
+        cannonical_image = src_image
+        if convert_to_black_white:
+            cannonical_image = self.convert_to_black_white(src_image)
+        return (cannonical_image.load(),cannonical_image.size[0],cannonical_image.size[1])
+
     def convert_to_black_white(self,src_image):
         gray_image = src_image.convert('L')
         return gray_image.point(lambda x: 0 if x<128 else 255, '1')
@@ -97,8 +116,10 @@ class ImageUtility:
         image = Image.open(image_src_path)
         extension = os.path.splitext(os.path.split(image_src_path)[-1])[1]
         image = self.trim(image,convert_to_black_white)
-        images = self.split_x(image,convert_to_black_white)
-        for i in range(len(images)):
+        images = self.split_x(image, True)
+        final_images = []
+        for image in images:
+            final_images.extend(self.split_y(image, True))
+        for i in range(len(final_images)):
             path = os.path.join(image_dest_dir_path,'trimmed_{}{}'.format(i,extension))
-            images[i].save(path)
-
+            final_images[i].save(path)
