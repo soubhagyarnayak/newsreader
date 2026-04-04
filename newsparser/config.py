69dfe1890eb13b2a1fd011cb618a055561badf67
soubhagya <soubhagya.r.nayak@gmail.com>
2026-04-04 13:16:26 +0530

    added updated config file based on .env entries
    added updated config file based on .env entries

--- newsparser/config.py
+++ newsparser/config.py
@@ -1,14 +1,13 @@
-DATABASE_CONFIG = {
-    'type': 'sqlite',
-    'dbname': 'feeds.db'
-}
+import os
+from dotenv import load_dotenv
 
-'''
+load_dotenv()
+
 DATABASE_CONFIG = {
-    'type': 'postgre',
-    'host': 'localhost',
-    'dbname': 'feeds',
-    'user':'user',
-    'password':'password',
+    'type': os.environ['DB_TYPE'],
+    'host': os.environ['DB_HOST'],
+    'dbname': os.environ['DB_NAME'],
+    'user': os.environ['DB_USER'],
+    'password': os.environ['DB_PASSWORD'],
 }
-'''
+QUEUE_CONNECTION_STRING = os.environ['QUEUE_CONNECTION_STRING']
